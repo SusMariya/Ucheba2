edaac1e0d7660e0711be2d9475dbe7deabd95c8e
Marya_S <suspitcina.marya@gmail.com>
2021-12-17 01:23:52 +0700

    stst metod

--- 22open.py
+++ 22open.py
@@ -395,63 +395,344 @@
 # rec1.get_drow()
 
 
-class Point:
-    WIDTH = 5
-    z = 100
+# class Point:
+#     WIDTH = 5
+#     z = 100
+#
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
 
-    def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
+# def __getattr__(self, item):
+#     return f"В классе {__class__.__name__} отсувует атрибут: {item}"
+#
+# def __getattribute__(self, item):
+#     if item =="_Point__x":
+#         return "Закрытая переменная"
+#     else:
+#     #         return object.__getattribute__(self, item)
+#     def __setattr__(self, key, value):
+#         if key == "WIDTH":
+#             raise AttributeError
+#         else:
+#             self.__dict__[key] = value
+#
+#     def check_val(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     def set_coords(self, x, y):
+#         if Point.check_val(x) and Point.check_val(y):
+#             self.__x = x
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def set_coords_x(self, x):
+#         if Point.check_val(x):
+#             self.__x = x
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def set_coords_y(self, y):
+#         if Point.check_val(y):
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами")
+#     def get_coords(self):
+#         return self.__x, self.__y
+#
+#     def area(self):
+#         return (self.__x*self.__y)+self.z
+#
+# p1 = Point(5, 10)
+# # print(p1.zzz)
+# # print(p1._Point_x)
+# # print(p1.get_coords())
+# # p1.set_coords(2, 3)
+# # print(p1.get_coords())
+# p1.z = 100
+# # Point.WIDTH = 10
+# print(p1.area())
 
-    # def __getattr__(self, item):
-    #     return f"В классе {__class__.__name__} отсувует атрибут: {item}"
-    #
-    # def __getattribute__(self, item):
-    #     if item =="_Point__x":
-    #         return "Закрытая переменная"
-    #     else:
-    #         return object.__getattribute__(self, item)
-    def __setattr__(self, key, value):
-        if key == "WIDTH":
-            raise AttributeError
-        else:
-            self.__dict__[key] = value
+# 16/12/2021
+# class Point:
+#     __slots__ = ["__x", "__y", "z"]
+#
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#
+# p1 = Point(5, 10)
+# p1.z = 1
+# print(p1.z)
+# print(p1.__dict__)
 
-    def check_val(z):
-        if isinstance(z, int) or isinstance(z, float):
-            return True
-        return False
 
-    def set_coords(self, x, y):
-        if Point.check_val(x) and Point.check_val(y):
-            self.__x = x
-            self.__y = y
-        else:
-            print("Координаты должны быть числами")
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __check_value(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     def __set_coords_x(self, x):
+#         if Point.__check_value(x):
+#             self.__x = x
+#         else:
+#             ValueError("Неверный формат данных")
+#
+#     def __get_coords_x(self):
+#         print("Вызывается __get_coords_x")
+#         return self.__x
+#
+#     def __del_coords_x(self):
+#         print("Удаление свойства")
+#         del self.__x
+#
+#     coordX = property(__get_coords_x, __set_coords_x, __del_coords_x)
+#
+#
+# p1 = Point(5, 10)
+# p1.coordX = 100
+# print(p1.coordX)
+# del p1.coordX
+# p1.coordX = 7
+# print(p1.coordX)
 
-    def set_coords_x(self, x):
-        if Point.check_val(x):
-            self.__x = x
-        else:
-            print("Координаты должны быть числами")
 
-    def set_coords_y(self, y):
-        if Point.check_val(y):
-            self.__y = y
-        else:
-            print("Координаты должны быть числами")
-    def get_coords(self):
-        return self.__x, self.__y
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __check_value(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     @property # геттер (getter) важен порядок
+#     def coords_x(self):
+#         print("Вызывается __get_coords_x")
+#         return self.__x
+#
+#     @coords_x.setter
+#     def coords_x(self, x):
+#         if Point.__check_value(x):
+#             self.__x = x
+#         else:
+#             ValueError("Неверный формат данных")
+#     @coords_x.deleter
+#     def coords_x(self):
+#         print("Удаление свойства")
+#         del self.__x
+#
+#     # coordX = property(__get_coords_x, __set_coords_x, __del_coords_x)
+#
+#
+# p1 = Point(5, 10)
+# p1.coords_x = 100
+# print(p1.coords_x)
+# del p1.coords_x
+# p1.coords_x = 7
+# # print(p1.coords_x)
+# print(p1.__dict__)
 
-    def area(self):
-        return (self.__x*self.__y)+self.z
+# задача перевест килогрммы в фунты
 
-p1 = Point(5, 10)
-# print(p1.zzz)
-# print(p1._Point_x)
-# print(p1.get_coords())
-# p1.set_coords(2, 3)
-# print(p1.get_coords())
-p1.z = 100
-# Point.WIDTH = 10
-print(p1.area())
+# class KgToPounds:
+#     def __init__(self, kg):
+#         self.__kg = kg
+#
+#     @property
+#     def kg(self):
+#         return self.__kg
+#
+#     @kg.setter
+#     def kg(self, new_kg):
+#         if isinstance(new_kg, (int, float)):
+#             self.__kg = new_kg
+#         else:
+#             print("Килограммы задаются только числами")
+#
+#     def to_pounds(self):
+#         return self.__kg * 2.205
+#
+# w = KgToPounds(12)
+# print(w.to_pounds())
+# w.kg = 41
+# print(w.kg)
+# print(w.to_pounds())
+# w.kg = "десять"
+# print(w.to_pounds())
+
+
+# class Point:
+#     __count = 0
+#
+#     def __init__(self, x=0, y=0):
+#         self.__x = x
+#         self.__y = y
+#         Point.__count+=1
+#
+#     @staticmethod
+#     def get_count():
+#         return Point.__count
+#
+# p1 = Point()
+# p2 = Point()
+# p3 = Point()
+#
+# print(Point.get_count())
+# print(p1.get_count())
+
+
+# class Change:
+#     @staticmethod
+#     def inc(x):
+#         return x+1
+#
+#     @staticmethod
+#     def dec(x):
+#         return x-1
+#
+# print(Change.inc(10), Change.dec(10))
+# q = Change()
+# print(q.dec(5), q.inc(5))
+
+# class Nambers:
+#     @staticmethod
+#     def max(x, y, z, w):
+#         if x>y and x>z and x>w:
+#             return "Максимальное число ", x
+#         elif y>x and y>z and y>w:
+#             return "Максимальное число ", y
+#         elif z>x and z>w and z>x:
+#             return "Максимальное число ", z
+#         else:
+#             return "Максимальное число ", w
+#     @staticmethod
+#     def min(x, y, z, w):
+#         if x<y and x<z and x<w:
+#             return "Минимальное число ", x
+#         elif y<x and y<z and y<w:
+#             return "Минимальное число ", y
+#         elif z<x and z<w and z<x:
+#             return "Минимальное число ", z
+#         else:
+#             return "Минимальное число ", w
+#
+#     @staticmethod
+#     def sred(x, y, z, w):
+#         return "Среднее значение", (x + y +z +w)/4
+#
+#     @staticmethod
+#     def faktorial(x):
+#         fac = 1
+#         for i in range(1, x + 1):
+#             fac *= i
+#         return f"Факориал числа", fac
+#
+#
+# print(Nambers.max(1, 2, 3, 4))
+# print(Nambers.min(1, 2, 3, 4))
+# print(Nambers.sred(1, 2, 3, 4))
+# print(Nambers.faktorial(4))
+
+# class Date:
+#     def __init__(self, day = 0, month = 0, year = 0):
+#         self.day = day
+#         self.month = month
+#         self.year = year
+#
+#     @classmethod
+#     def from_string(cls, date_as_string):
+#         day, month, year = map(int, date_as_string.split("."))
+#         date1 = cls(day, month, year)
+#         return date1
+#
+#     def string_to_db(self):
+#         return f'{self.year}-{self.month}-{self.day}'
+#
+# # d = "16.12.2021"
+# # day, month, year = map(int, d.split("."))
+# # print(day, month, year)
+# # date = Date(day, month, year)
+# date = Date.from_string("16.12.2021")
+# print(date.string_to_db())
+# date1 = Date.from_string("17.12.2021")
+# print(date1.string_to_db())
+
+# class Date:
+#     def __init__(self, day = 0, month = 0, year = 0):
+#         self.day = day
+#         self.month = month
+#         self.year = year
+#
+#     @classmethod
+#     def from_string(cls, date_as_string):
+#         day, month, year = map(int, date_as_string.split("."))
+#         date1 = cls(day, month, year)
+#         return date1
+#
+#     @staticmethod
+#     def is_date_valid(date_as_string):
+#         if date_as_string.count('.') == 2:
+#             day, month, year = map(int, date_as_string.split("."))
+#             return day <= 31 and month <= 12 and year <= 3999
+#
+#     def string_to_db(self):
+#         return f'{self.year}-{self.month}-{self.day}'
+# date =[
+#     '30.12.2021'
+#     '38-12-2020'
+#     '01.01.2021'
+#     '12.31.2020'
+# ]
+#
+# for d in dates:
+#     if Date.is_date_valid(d):
+#         date = Date.from_string(d)
+#         db = date.string_to_db()
+#         print(db)
+#     else:
+#         print("Неправильный формат даты")
+#
+
+
+class Ploshad:
+    count = 0
+    @staticmethod
+    def S_Geron(a, b, c):
+        Ploshad.count +=1
+        perimetr = (a+b+c)/2
+        return (perimetr*(perimetr-a) * (perimetr-b) * (perimetr-c))**0.5
+
+    @staticmethod
+    def s_Treyg(a, h):
+        Ploshad.count += 1
+        return 0.5* h * a
+
+    @staticmethod
+    def S_sq(e):
+        Ploshad.count += 1
+        return e**2
+
+    @staticmethod
+    def S_pramoyg(a, b):
+        Ploshad.count += 1
+        return a*b
+
+
+print(Ploshad.S_Geron(3, 4, 5))
+print(Ploshad.s_Treyg(6, 7))
+print(Ploshad.S_sq(7))
+print(Ploshad.S_pramoyg(2, 6))
+try_t = Ploshad()
+print(try_t.S_Geron(1, 2, 3))
+print(Ploshad.count)
